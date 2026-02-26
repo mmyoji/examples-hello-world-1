e5dd9165ceaed848d120e3411de2a3620787472f
mmyoji <mmyoji@gmail.com>
2026-02-26 10:12:29 +0900

    add blank manifest.json

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
+    const path = (new URL(req.url).pathname);
+    if (path === "/manifest.json") {
+        return serveFile(req, "./manifest.json");
+    }
+
     return serveFile(req, "./index.html");
 });
